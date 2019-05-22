a5640ecafb541102a2aecc1d77f53ef6b82c06b7
ferhatdinc34 <ferhatdinc34@gmail.com>
2019-05-22 22:27:08 +0300

    Company Login Done

--- CarajRestfulApp/Forms/FrmMain.cs
+++ CarajRestfulApp/Forms/FrmMain.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using CarajRestfulApp.Models;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,9 +17,10 @@ namespace CarajRestfulApp.Forms
     {
         private Dictionary<Type, Form> ActiveForms;
         public static Boolean isLogin = false;
-
+        public static JObject company;//key value
         public FrmMain()
         {
+
             InitializeComponent();
             this.ActiveForms = new Dictionary<Type, Form>();
         }
@@ -48,10 +52,25 @@ namespace CarajRestfulApp.Forms
             this.ShowForm<FrmReservationDetails>();
 
         }
-
-        private void btnLogin_Click(object sender, EventArgs e)
+        string link = "http://165.22.91.48/api/";
+        private async void BtnLogin_ClickAsync(object sender, EventArgs e)
         {
-            this.gbLogin.Visible = false;
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(
+                    link + "auth/CompanyLogin?companyname=" + txtCompanyName.Text + "&password=" + txtPassword.Text,
+                     new StringContent(txtCompanyName.Text, Encoding.UTF8, "application/json"));
+
+                if (response.StatusCode.GetHashCode()==200)
+                {
+                    var model = response.Content.ReadAsAsync<JObject>().Result;
+                    // company.CompanyID = model.Company;
+                    company = model;
+                    MessageBox.Show("Hoşgeldin "+company["companyID"]);//company name olarak değişecek
+                    this.gbLogin.Visible = false;
+                }
+            }
+            
         }
     }
 }
